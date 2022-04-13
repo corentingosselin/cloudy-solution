f9ad3bba7646943db39bf3ea4d904931e4445e01
Prodigyman <corentingosselin@hotmail.fr>
2022-04-13 22:57:05 +0200

    dtos

---
+++ libs/shared/data-access/src/lib/dto/error.dto.ts
@@ -0,0 +1,5 @@
+export interface ErrorDto {
+    field: string;
+    message: string;
+}
+        
---
+++ libs/shared/data-access/src/lib/dto/login.dto.ts
@@ -0,0 +1,8 @@
+export interface LoginDto {
+    id: string;
+    email: string;
+    firstname: string;
+    lastname: string;
+    access_token: string;
+}
+        
